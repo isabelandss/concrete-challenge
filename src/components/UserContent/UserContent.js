e593923a2fb58faed0c3bf8024b1fcb1b472e065
isabelandss <isabelandss@gmail.com>
2018-06-29 14:11:54 -0300

    Estilizando UserContent

--- src/components/UserContent/UserContent.js
+++ src/components/UserContent/UserContent.js
@@ -4,6 +4,11 @@ import './style.css';
 const UserContent = (props) => 
   <aside>
     <img className="avatar" src="https://avatars0.githubusercontent.com/u/21112599?s=460&v=4" alt="" />
+    <p className="user-name">Isabela Neri dos Santos</p>
+    <p className="user-login">isabelandss</p>
+    <p>Concrete Solutions</p>
+    <p>90 seguidores</p>
+    <p>45 seguindo</p>
   </aside>
 
 export default UserContent;
