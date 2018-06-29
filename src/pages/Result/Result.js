edc96199709fbe7e57cf5cbf95ae352050d2ccbe
isabelandss <isabelandss@gmail.com>
2018-06-29 17:56:44 -0300

    Preparando componente e page para receber dados

--- src/pages/Result/Result.js
+++ src/pages/Result/Result.js
@@ -4,18 +4,40 @@ import UserContent from '../../components/UserContent';
 import RepositoryContent from '../../components/RepositoryContent';
 import Footer from '../../components/Footer';
 import './style.css';
+import { userService } from '../../services/user';
 
 class Result extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor() {
+    super();
+
+    this.state = {
+      user: {},
+      repos: {}
+    };
+  }
+
+  async componentDidMount() {
+    const user = await userService.getUser('barbaromatrix');
+    console.log(user);
+
+    this.setState({
+      user
+    });
+  }
 
   render() {
     return (
       <div>
         <Header />
         <div className="wrapper">
-          <UserContent />
+          <UserContent 
+            name={this.state.user.name} 
+            login={this.state.user.login}
+            avatar_url={this.state.user.avatar_url}  
+            followers={this.state.user.followers}
+            following={this.state.user.following}
+            bio={this.state.user.bio}
+          />
           <section>
             <RepositoryContent />
             <RepositoryContent />
