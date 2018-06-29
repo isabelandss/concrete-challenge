300245e90391363ca61c93eb3a37ef1d7ff47c4e
isabelandss <isabelandss@gmail.com>
2018-06-29 12:25:30 -0300

    Estrutura básica e inicial do input

---
+++ src/components/Input/Input.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import 'style.css';
+
+const Input = () => 
+  <form action="">
+    <input type="text" />
+    <button><i classNames="material-icons">search</i></button>
+  </form>
+
+export default Input;
