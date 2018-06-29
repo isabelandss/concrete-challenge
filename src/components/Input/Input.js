22cb661c6fbcc6d6aff872032306c5d34ddf7d30
isabelandss <isabelandss@gmail.com>
2018-06-29 13:46:09 -0300

    Estilizando o header

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import 'style.css';
+import './style.css';
 
 const Input = () => 
   <form action="">
     <input type="text" />
-    <button><i classNames="material-icons">search</i></button>
+    <button><i className="material-icons">search</i></button>
   </form>
 
 export default Input;
--- src/components/UserContent/UserContent.js
+++ src/components/UserContent/UserContent.js
@@ -3,7 +3,7 @@ import './style.css';
 
 const UserContent = (props) => 
   <aside>
-    <img src="https://avatars0.githubusercontent.com/u/21112599?s=460&v=4" alt="" />
+    <img className="avatar" src="https://avatars0.githubusercontent.com/u/21112599?s=460&v=4" alt="" />
   </aside>
 
 export default UserContent;
