a7f169f57029d1a304c44cf5c9920897f0f85387
isabelandss <isabelandss@gmail.com>
2018-06-29 17:10:29 -0300

    Estilizando footer e cards

--- src/components/RepositoryContent/RepositoryContent.js
+++ src/components/RepositoryContent/RepositoryContent.js
@@ -2,6 +2,17 @@ import React from 'react';
 import './style.css';
 
 const RepositoryContent = (props) => 
-  <article></article>
+  <article>
+    <header className="title-repo">
+      concrete-challenge
+    </header>
+    <p className="description-repo">
+      Sed sed lacus ex. Maecenas eget sodales nisi, sit amet aliquet tortor. Mauris auctor feugiat imperdiet.
+    </p>
+    <div className="rate-repo">
+      <i className="material-icons">star_border</i>
+      <p className="quantity">7</p>
+    </div>
+  </article>
 
 export default RepositoryContent;
