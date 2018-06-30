031201e8c0169bcf3af5c548d069060454eeea90
isabelandss <isabelandss@gmail.com>
2018-06-30 12:17:38 -0300

    Configurando pra capiturar dados

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './style.css';
 
-const Input = () => 
+const Input = ({ onChange, onClick }) => 
   <form action="">
-    <input type="text" />
-    <button><i className="material-icons">search</i></button>
+    <input type="text" name="search" onChange={onChange}/>
+    <button onClick={onClick}><i className="material-icons">search</i></button>
   </form>
 
 export default Input;
