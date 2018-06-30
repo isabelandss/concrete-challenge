6dabad1493ee5284f9c5ae8d4aca1442233258b7
isabelandss <isabelandss@gmail.com>
2018-06-30 13:49:25 -0300

    Desenvolvimento página Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
+import Input from '../../components/Input';
+import { userService } from '../../services/user';
+import { repoService } from '../../services/repo';
+import './style.css';
 
 class Home extends Component {
   constructor(props) {
     super(props);
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      user: {},
+      repos: []
+    }
+  }
+
+  onChange(event) {
+    const { value } = event.target;
+    this.setState({ search: value });
+  }
+
+  async onSubmit(event) {
+    event.preventDefault();
+    const user = await userService.getUser(this.state.search);
+    const repos = await repoService.getRepos(this.state.search);
+
+    this.props.history.push('/result', { user, repos });
   }
 
   render() {
     return (
-      <div>Home</div>
+      <div className="home-wrapper">
+        <div className="home-search">
+          <p>Github<span>Search</span></p>
+          <Input onChange={this.onChange} onSubmit={this.onSubmit}/>
+        </div>
+      </div>
     );
   }
 }
