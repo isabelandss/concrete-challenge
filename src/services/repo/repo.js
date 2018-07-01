e358a41d3e994751fa9265488ae066b511864fac
isabelandss <isabelandss@gmail.com>
2018-07-01 14:32:24 -0300

    Devido ao erro de rate limit, foi definido access_token nos serviços

--- src/services/repo/repo.js
+++ src/services/repo/repo.js
@@ -1,8 +1,14 @@
+const token = '5fcf426202fc4088a3164c76e3a3570ec391517d';
+
 export default ({request}) => ({
   getRepos: async username => {
-    const baseUrl = 'https://api.github.com/users';
-    const { data } = await request.get(`${baseUrl}/${username}/repos`);
-    
-    return data;
+    try {
+      const baseUrl = 'https://api.github.com/users';
+      const { data } = await request.get(`${baseUrl}/${username}/repos?access_token=${token}`);
+      
+      return data;
+    } catch (error) {
+      return error.response;
+    }
   }
 });
