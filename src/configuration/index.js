d82a1e063e4b39d069e9e6bd40c7b656c0fb9627
isabelandss <isabelandss@gmail.com>
2018-07-01 20:15:19 -0300

    Alterando chave de configuração. Após fazer commit, a chave de autorização parece ficar vencida

--- src/configuration/index.js
+++ src/configuration/index.js
@@ -1,6 +1,6 @@
 const config = {
   github_api_url: 'https://api.github.com',
-  access_token: '13f60e6134761460af11ee92e155a966e4347f53'
+  access_token: 'ace9e018f4e5278116cc5726574a3e6b2c3d3ab9'
 };
 
 export default config;
